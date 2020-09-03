859b45681f2ecdf91f093f229e084223fda9ea59
Tony3641 <tony3641@126.com>
2020-09-03 20:12:33 +0800

    Init commit.

---
+++ Test/ConsoleApp2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String aFriend = "Tony";
+            Console.WriteLine($"Hello {aFriend}\nThis name has {aFriend.Length} chars.");
+
+            string greeting = "      Hello World!       ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
+            string songLyrics2 = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics2.StartsWith("You"));
+            Console.WriteLine(songLyrics2.StartsWith("goodbye"));
+
+            Console.WriteLine(songLyrics2.EndsWith("hello"));
+            Console.WriteLine(songLyrics2.EndsWith("goodbye"));
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            decimal min2 = decimal.MinValue;
+            decimal max2 = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min2} to {max2}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine($"My name is {names[0]}.");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+        }
+    }
+}
