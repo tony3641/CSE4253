ca333e010a7adbab91a73b103a666d21442a92ff
Tony3641 <tony3641@126.com>
2020-09-14 04:01:10 +0800

    Add Assignment1

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Assignment1
+{
+    class Program
+    {
+        [System.STAThread]
+        static void Main(string[] args)
+        {
+            char[] separators = new char[] { ',', '.', ':' ,';','!','?',' ','\n','[',']','"','\'','(',')'};
+            String[] words = new String[1000];
+            String clipStr="aslkjf!jfkdsajf.fasdjfl";
+            bool hasString = Clipboard.ContainsText();
+            if (hasString)
+            {
+                clipStr = Clipboard.GetText(TextDataFormat.UnicodeText);
+            }
+            words = clipStr.Split(separators, 1000);
+
+            Array.Sort(words);
+
+            foreach (var item in words)
+            {
+                foreach (var sep in separators)
+                {
+                    if (item.Contains(sep))
+                    {
+                        item.Remove(sep);
+                    }
+                }
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
